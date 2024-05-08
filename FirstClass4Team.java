f2bd5d785c701c050a70e6d9ef95b3be257603b4
YIHUI <112804328+yihui1227@users.noreply.github.com>
2024-05-08 16:01:53 +0800

    新增連怡卉資料
    :)

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號:\"01157029\"\n姓名:\"謝心瑜\"");
         System.out.println("\"學號:\"01157014\"\n姓名:\"蘇盈睿\"");
+        System.out.println("\"學號:\"01157027\"\n姓名:\"連怡卉\"");
     }
 }
